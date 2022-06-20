91108cf7313cc11a85a3d66763c693914063af7f
Oleksandr Raspopov <107881921+SashaRasppv@users.noreply.github.com>
2022-06-20 23:28:47 +0200

    firebase refactor

--- lib/provider/firebase_database/provider_firebase.dart
+++ lib/provider/firebase_database/provider_firebase.dart
@@ -4,19 +4,20 @@ import 'package:flutter/material.dart';
 
 class ProviderFirebase extends ChangeNotifier {
   Future<void> addCoin(String coinName, double amount) async {
-    DocumentSnapshot<Map<String, dynamic>> documnet = await FirebaseFirestore
+    
+    DocumentSnapshot<Map<String, dynamic>> document = await FirebaseFirestore
         .instance
         .collection('Coins')
         .doc(coinName)
         .get();
+    
     try {
-      if (!documnet.exists) {
-        print('is empty');
+      if (!document.exists) {
         FirebaseFirestore.instance
             .collection('Coins')
             .doc(coinName)
-            .set({'Amount': amount});
-      } else {
+            .set({'Amount': amount});        
+      } else {     
         FirebaseFirestore.instance.runTransaction((transaction) async {
           DocumentSnapshot snapshot = await transaction.get(
               FirebaseFirestore.instance.collection('Coins').doc(coinName));
@@ -24,26 +25,11 @@ class ProviderFirebase extends ChangeNotifier {
           transaction.update(
               FirebaseFirestore.instance.collection('Coins').doc(coinName),
               {'Amount': newAmout});
-          return true;
         });
-        print('is not empty');
-      }
+      }     
     } catch (e) {
       print(e);
     }
+    
   }
 }
-//   Future<void> updateCoin(String coinName, String amount) async {
-//     try {
-//       FirebaseFirestore.instance.collection('Coins').doc(coinName).get();
-//           FirebaseFirestore.instance.runTransaction((transaction) async {
-//       DocumentSnapshot snapshot = await transaction.get(FirebaseFirestore.instance.collection('Coins').doc(coinName));
-//       if (!snapshot.exists) {
-//         FirebaseFirestore.instance.collection('Coins').doc(coinName).set({'Amount': amount});
-//         return true;
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-// }
