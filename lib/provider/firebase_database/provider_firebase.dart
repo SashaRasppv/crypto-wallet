899aa456b46151496ee142525a8227eaade73698
Oleksandr Raspopov <olexandrraspopov@gmail.com>
2022-06-20 23:04:18 +0200

    firebase update

--- lib/provider/firebase_database/provider_firebase.dart
+++ lib/provider/firebase_database/provider_firebase.dart
@@ -4,33 +4,46 @@ import 'package:flutter/material.dart';
 
 class ProviderFirebase extends ChangeNotifier {
   Future<void> addCoin(String coinName, double amount) async {
-    CollectionReference collectionReference = FirebaseFirestore.instance
+    DocumentSnapshot<Map<String, dynamic>> documnet = await FirebaseFirestore
+        .instance
         .collection('Coins')
         .doc(coinName)
-        .collection('Amount');
-    QuerySnapshot snapshot = await collectionReference.get();
+        .get();
     try {
-      if (snapshot.docs.isEmpty) {
+      if (!documnet.exists) {
+        print('is empty');
         FirebaseFirestore.instance
             .collection('Coins')
             .doc(coinName)
             .set({'Amount': amount});
       } else {
+        FirebaseFirestore.instance.runTransaction((transaction) async {
+          DocumentSnapshot snapshot = await transaction.get(
+              FirebaseFirestore.instance.collection('Coins').doc(coinName));
+          double newAmout = snapshot.get('Amount') + amount;
+          transaction.update(
+              FirebaseFirestore.instance.collection('Coins').doc(coinName),
+              {'Amount': newAmout});
+          return true;
+        });
         print('is not empty');
       }
     } catch (e) {
       print(e);
     }
   }
-
-  Future<void> updateCoin(String coinName, String amount) async {
-    try {
-      FirebaseFirestore.instance
-          .collection('Coins')
-          .doc(coinName)
-          .update({'Amount': amount});
-    } catch (e) {
-      print(e);
-    }
-  }
 }
+//   Future<void> updateCoin(String coinName, String amount) async {
+//     try {
+//       FirebaseFirestore.instance.collection('Coins').doc(coinName).get();
+//           FirebaseFirestore.instance.runTransaction((transaction) async {
+//       DocumentSnapshot snapshot = await transaction.get(FirebaseFirestore.instance.collection('Coins').doc(coinName));
+//       if (!snapshot.exists) {
+//         FirebaseFirestore.instance.collection('Coins').doc(coinName).set({'Amount': amount});
+//         return true;
+//       }
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
